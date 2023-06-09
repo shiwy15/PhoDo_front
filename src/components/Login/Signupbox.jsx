4b55a1f4d0405ddf50f1754036d666d07e51d7b8
hodee <tlsghk8619@gmail.com>
2023-06-10 04:01:02 +0900

    feat: config & signup axios set

--- src/components/Login/Signupbox.jsx
+++ src/components/Login/Signupbox.jsx
@@ -1,13 +1,50 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
+import {API} from "../../config";
+import { useNavigate } from 'react-router-dom';
 
 const Signupbox = () => {
+    const navigate = useNavigate();
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        error: '',
+        success: false
+    });
+
+    const { name, email, password, error } = formData;
+
+    console.log(formData);
+    
+    const handleChange = text => e => {
+        setFormData({ ...formData, [text]: e.target.value });
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        axios.post(`${API}/signup`, {
+            name,
+            email,
+            password
+        }).then(res => {
+            if (res.status === 200) {
+                setFormData({ ...formData, error: '', success: true });
+                navigate('/login');
+            }
+        }).catch(err => {
+            setFormData({ ...formData, error: err.response.data.error, success: false });
+        });
+    };
+    
     return (
         <div className="relative flex flex-col justify-center min-h-screen overflow-hidden">
             <div className="w-full p-6 m-auto bg-white rounded-md shadow-xl lg:max-w-xl">
                 <h1 className="text-4xl font-semibold text-center text-purple-700">
                    Sign up
                 </h1>
-                <form className="mt-6">
+                {error && <div className="alert alert-danger">{error}</div>}
+                <form className="mt-6" onSubmit={handleSubmit}>
                     <div className="mb-2">
                         <label
                             htmlFor="name"
@@ -17,6 +54,8 @@ const Signupbox = () => {
                         </label>
                         <input
                             type="name"
+                            value={name}
+                            onChange={handleChange('name')}
                             className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
                         />
                     </div>
@@ -29,6 +68,8 @@ const Signupbox = () => {
                         </label>
                         <input
                             type="email"
+                            value={email}
+                            onChange={handleChange('email')}
                             className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
                         />
                     </div>
@@ -41,6 +82,8 @@ const Signupbox = () => {
                         </label>
                         <input
                             type="password"
+                            value={password}
+                            onChange={handleChange('password')}
                             className="block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-purple-400 focus:ring-purple-300 focus:outline-none focus:ring focus:ring-opacity-40"
                         />
                     </div>
---
+++ src/config.js
@@ -0,0 +1,9 @@
+const BASE_URL = 'http://localhost:4000';
+
+export const API = {
+  MAIN: `${BASE_URL}/main`,
+  SIGNUP: `${BASE_URL}/signup`,
+  LOGIN: `${BASE_URL}/login`,
+  FORGOTPW: `${BASE_URL}/forgotpw`,
+  MYPAGE: `${BASE_URL}/mypage`,
+};
