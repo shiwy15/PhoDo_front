1f350d68bb9e93c954263a8f2a85d24715542b97
jinkyo <wlsry0910@gmail.com>
2023-06-10 00:22:22 +0900

    feat: first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+import Login from "./pages/Login/Login.jsx";
+import { Routes, Route } from "react-router-dom"
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
+
+
+const queryClient = new QueryClient()
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+// RENDER APP
+root.render(
+    <React.StrictMode>
+        <QueryClientProvider client = {queryClient}>
+            <BrowserRouter>
+            <App />
+            <ReactQueryDevtools />
+            </BrowserRouter>
+        </QueryClientProvider>
+    </React.StrictMode>
+);
+
