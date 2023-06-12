5213b16c1627510731038bf7798b61a9d0b1fb89
hodee <tlsghk8619@gmail.com>
2023-06-12 13:36:22 +0900

    feat: server.json setting & practice node page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Passwordchange from "./pages/Login/Passwordchange.jsx";
 //editor set
 import Editor from "./pages/Editor/Editor.jsx"
 
+//react query practice set
+import Practice from "./pages/Practice/Practice.jsx"
+
 import { Routes, Route } from "react-router-dom"
 
 function App() {
@@ -21,6 +24,11 @@ function App() {
 
         {/* editor set */}
         <Route path="/editor" element={<Editor />} />
+
+        {/* react query sample */}
+        <Route path="/practice" element={<Practice />} />
+
+
       </Routes>
     </div>
   );
