5efb623c8f7288f53908e693f3ea9eba7d246dde
jinkyo <wlsry0910@gmail.com>
2023-06-15 07:36:38 +0900

    feat:menubar

--- src/components/Main/MainPost.jsx
+++ src/components/Main/MainPost.jsx
@@ -7,7 +7,7 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 
 
-function MainFeaturedPost() {
+const MainFeaturedPost =() => {
   const post = {
     title: 'Photo 업무를 함께 DO!',
     description: '서로 어떻게 공유하느냐가 업무 효율을 좌우합니다.\n프로젝트 멤버들에게 일일이 묻고 물어 저 멀리 자료를 찾아 삼만리를 떠나던 과거는 이제 안녕!\nPhoDo는 효율이 중요한 당신을 위한 사진 중심 협업 사이트입니다.',
@@ -53,7 +53,7 @@ function MainFeaturedPost() {
               pr: { md: 2 },
             }}
           >
-            <Typography component="h1" variant="h3" color="inherit" gutterBottom sx={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>
+            <Typography variant="h2" color="inherit" gutterBottom sx={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>
               {post.title}
             </Typography>
             <Typography variant="h6" color="inherit" paragraph sx={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>
@@ -64,7 +64,7 @@ function MainFeaturedPost() {
                 </React.Fragment>
             ))}
             </Typography>
-            <Button variant="outlined" href="/editor" sx={{color: 'white', borderColor: 'white', borderRadius: '50px'}}>
+            <Button variant="outlined" href="/newproject" sx={{color: 'white', borderColor: 'white', borderRadius: '50px'}}>
             {post.linkText}
             </Button>
 
--- src/pages/Editor/Editor2.jsx
+++ src/pages/Editor/Editor2.jsx
@@ -3,11 +3,15 @@ import Editingbox2 from "../../components/Editor/Editingbox2";
 // import Sidebar from "../../components/Editor/Sidebar";
 
 
+//components
+import MenuBar from "../../components/Editor/MenuBarR";
+
 
 function Editor2() {
     return (
         <div className="edit">
             <Editingbox2/>
+            <MenuBar />
         </div>
     )
 }
