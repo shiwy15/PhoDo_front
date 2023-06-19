3379a1a78cd7b3bafdbb95b00189c43482e4fe7a
hodee <tlsghk8619@gmail.com>
2023-06-19 20:47:34 +0900

    feat: console fix

--- src/components/Editor/ydoc.jsx
+++ src/components/Editor/ydoc.jsx
@@ -1,26 +1,27 @@
-// import { Doc } from 'yjs';
-// // For this example we use the WebrtcProvider to synchronize the document
-// // between multiple clients. Other providers are available.
-// // You can find a list here: https://docs.yjs.dev/ecosystem/connection-provider
-// // import { WebrtcProvider } from 'y-webrtc';
-// import { WebsocketProvider } from 'y-websocket';
+import { Doc } from 'yjs';
+// For this example we use the WebrtcProvider to synchronize the document
+// between multiple clients. Other providers are available.
+// You can find a list here: https://docs.yjs.dev/ecosystem/connection-provider
+// import { WebrtcProvider } from 'y-webrtc';
+import { WebsocketProvider } from 'y-websocket';
 
 
-// const ydoc = new Doc();
+const ydoc = new Doc();
 
-// // new WebrtcProvider('REACTFLOW-COLLAB-EXAMPLE', ydoc, {
-// //     signaling : ['ws://localhost:3000/newproject']
-// // });
+// new WebrtcProvider('REACTFLOW-COLLAB-EXAMPLE', ydoc, {
+//     signaling : ['ws://localhost:3000/newproject']
+// });
 
-// const wsProvider = new WebsocketProvider(
-//     'ws://13.125.210.252:1234',
-//     'abc',
-//     ydoc
-// );
+const wsProvider = new WebsocketProvider(
+    // 'ws://13.125.210.252:1234', //🔥 ec2 setting
+    'ws://localhost:1234', //🔥 ec2 setting
+    'abc',
+    ydoc
+);
 
-// wsProvider.on('status', event => {
-//     // console.log(event)
-//     // console.log(event.status)
-// })
+wsProvider.on('status', event => {
+    // console.log(event)
+    // console.log(event.status)
+})
 
-// export default ydoc;
+export default ydoc;
