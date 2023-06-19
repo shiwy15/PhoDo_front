ab143e285a84a55e478fb9c56e6e59a7494475e5
hodee <tlsghk8619@gmail.com>
2023-06-19 11:47:07 +0900

    feat: working at diff browser

--- src/components/Editor/ydoc.jsx
+++ src/components/Editor/ydoc.jsx
@@ -2,9 +2,21 @@ import { Doc } from 'yjs';
 // For this example we use the WebrtcProvider to synchronize the document
 // between multiple clients. Other providers are available.
 // You can find a list here: https://docs.yjs.dev/ecosystem/connection-provider
-import { WebrtcProvider } from 'y-webrtc';
+// import { WebrtcProvider } from 'y-webrtc';
+import { WebsocketProvider } from 'y-websocket';
+
 
 const ydoc = new Doc();
-new WebrtcProvider('REACTFLOW-COLLAB-EXAMPLE', ydoc);
+
+// new WebrtcProvider('REACTFLOW-COLLAB-EXAMPLE', ydoc, {
+//     signaling : ['ws://localhost:3000/newproject']
+// });
+
+new WebsocketProvider(
+    'ws://localhost:1234',
+    'REACTFLOW-COLLAB-EXAMPLE',
+    ydoc
+);
+
 
 export default ydoc;
