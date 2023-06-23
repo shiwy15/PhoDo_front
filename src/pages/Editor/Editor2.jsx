bc355b4104116fc1e5e8e8547f8aa1ac56a5ed23
hodee <tlsghk8619@gmail.com>
2023-06-23 21:01:00 +0900

    feat: new project params

--- src/pages/Editor/Editor2.jsx
+++ src/pages/Editor/Editor2.jsx
@@ -1,14 +1,20 @@
-import React from "react";
-
-import Editingbox2 from "../../components/Editor/Editingbox2";
+import React, {useEffect} from "react";
+// import Editingbox2 from "../../components/Editor/Editingbox2";
 import Header from '../../components/mypage/Header';
+import { useParams } from "react-router-dom";
 
 function Editor2() {
+    const {projectId} = useParams();
+    useEffect(()=> {
+        console.log(projectId);
+    }, [projectId]);
+
     return (
         <div className="edit">
             <Header />
-            <Editingbox2 />
+            {projectId} project created!
         </div>
     )
 }
+
 export default Editor2;
--- src/pages/Modal/Modal.jsx
+++ src/pages/Modal/Modal.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { API } from '../../utils/config';
+
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
