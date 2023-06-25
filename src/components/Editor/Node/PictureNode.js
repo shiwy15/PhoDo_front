778bb798d3a01effcde8e139767cda6eb2efe6e8
hodee <tlsghk8619@gmail.com>
2023-06-26 01:12:02 +0900

    tags out in pix

--- src/components/Editor/Node/PictureNode.js
+++ src/components/Editor/Node/PictureNode.js
@@ -12,14 +12,14 @@ function PictureNode({ data, isConnectable }) {
   return (
     <div className="PictureNodeblock rounded-lg bg-white text-center shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 pt-2">
       <Handle type="target" position={Position.Left} id="left" isConnectable={isConnectable} />
-      {
+      {/* {
         // Render each tag as a separate label
         data.tags.split(',').map((tag, index) => (
           <label key={index} htmlFor="text" className="text-center">
             Tag: {tag.trim()} 
           </label>
         ))
-      }
+      } */}
       <img src={data.url} alt="image" style={{ width: "300px" }} />
       <Handle type="source" position={Position.Right} id="right" isConnectable={isConnectable} />
     </div>
