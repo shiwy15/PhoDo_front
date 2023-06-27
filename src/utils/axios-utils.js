9654425cfa045f2f3aa3e0ece6dac8fbced31885
jinkyo <wlsry0910@gmail.com>
2023-06-27 12:32:40 +0900

    fix: merge fix

--- src/utils/axios-utils.js
+++ src/utils/axios-utils.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 //백엔드 서버 연결
-//const client = axios.create({ baseURL : 'https://hyeontae.shop'})
+const client = axios.create({ baseURL : 'https://hyeontae.shop'})
 
 //json server 등 연결
-const client = axios.create({ baseURL : 'http://localhost:4000'})
+// const client = axios.create({ baseURL : 'http://localhost:4000'})
 
 export const request = ({ ...options}) => {
     client.defaults.headers.common.Authorization = `Bearer token`
