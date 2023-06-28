55b3019c78468a42511b7a77bf949cc34454145f
shiwy15 <shiwy15@gmail.com>
2023-06-28 22:28:19 +0900

    메인 페이지 타이포그래피 변경

--- src/components/Main/exampleCard.jsx
+++ src/components/Main/exampleCard.jsx
@@ -3,42 +3,42 @@ import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
-const data = [
-  {
-    src: '/node_ex.png',
-    title: '타임라인 노드',
-    channel: '업무 진행 타임라인에 따른 노드를 만들어 봐요!',
-    views: '130M views',
-    createdAt: '2023.09.10',
-  },
-  {
-    src: '/venndia.png',
-    title: '벤 다이어그램',
-    channel: '같은 태그들의 사진을 벤 다이어그램을 만들어요!',
-    views: '396k views',
-    createdAt: '2023.06.15',
-  },
-  {
-    src: '/sunb.png',
-    title: '썬 버스트',
-    channel: '데이터의 계층 구조와 비중을 한눈에 만들어 봐요!',
-    views: '40M views',
-    createdAt: '2023.06.13',
-  },
-{
-    src: '/timeline2.png',
-    title: '스케쥴링',
-    channel: '업무의 흐름을 시각화하고 일정을 만들어 봐요!',
-    views: '130M views',
-    createdAt: '2023.06.09',
-  },
-];
+// const data = [
+//   {
+//     src: '/node_ex.png',
+//     title: '타임라인 노드',
+//     channel: '업무 진행 타임라인에 따른 노드를 만들어 봐요!',
+//     views: '130M views',
+//     createdAt: '2023.09.10',
+//   },
+//   {
+//     src: '/venndia.png',
+//     title: '벤 다이어그램',
+//     channel: '같은 태그들의 사진을 벤 다이어그램을 만들어요!',
+//     views: '396k views',
+//     createdAt: '2023.06.15',
+//   },
+//   {
+//     src: '/sunb.png',
+//     title: '썬 버스트',
+//     channel: '데이터의 계층 구조와 비중을 한눈에 만들어 봐요!',
+//     views: '40M views',
+//     createdAt: '2023.06.13',
+//   },
+// {
+//     src: '/timeline2.png',
+//     title: '스케쥴링',
+//     channel: '업무의 흐름을 시각화하고 일정을 만들어 봐요!',
+//     views: '130M views',
+//     createdAt: '2023.06.09',
+//   },
+// ];
 
 export const ExampleCard = () => {
   return (
     <>
     <Typography gutterBottom variant="h6" sx={{ marginLeft: 6, textDecoration: 'underline' }}>Phodo와 함께하면 할 수 있는 것!</Typography>
-    <Grid container wrap="nowrap" justifyContent="center">
+    {/* <Grid container wrap="nowrap" justifyContent="center">
       {data.map((item, index) => (
         <Box key={index} sx={{ marginLeft: 4, width: '400', mx: 2  }}>
           <img
@@ -57,10 +57,10 @@ export const ExampleCard = () => {
             {/* <Typography variant="caption" color="text.secondary">
               {`${item.views} • ${item.createdAt}`}
             </Typography> */}
-          </Box>
+          {/* </Box>
         </Box>
       ))}
-    </Grid>
+    </Grid> */} 
     </>
   );
 }
