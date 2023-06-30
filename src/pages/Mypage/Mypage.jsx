091899239d7cdaf4d856882ff0b3ba25d4012a02
jinkyo <wlsry0910@gmail.com>
2023-06-30 20:07:09 +0900

    feat: css3

--- src/pages/Mypage/Mypage.jsx
+++ src/pages/Mypage/Mypage.jsx
@@ -10,16 +10,7 @@ import DetailShow from "../../components/mypage/DetailShow"
 const Mypage = () =>{
   return (
     <Container fluid className="project-section">
-
-  {/* <div className="fixed w-60 left-0"> 
-  < SidebarL /> 
-  </div> */}
-  {/* <div className="fixed flex flex-wrap justify-center right-0 w-96 h-screen  bg-white shadow-[0_4px_12px_0_rgba(0,0,0,0.07),_0_2px_4px_rgba(0,0,0,0.05)]">
-    <ImgFileInput />
-    <div className="self-center w-4/5 h-0.5 bg-gray-300 my-2"></div> 
-    <DetailShow />
-  </div> */}
-  <GalleryArea2 />
+      <GalleryArea2/>
     </Container>
   );
 }
