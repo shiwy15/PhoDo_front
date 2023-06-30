8b41731c8ce7ee874bedc7cd83131d0e6bc83573
jinkyo <wlsry0910@gmail.com>
2023-06-30 17:13:35 +0900

    fix: merge resolve - remove header

--- src/pages/Report/Report.jsx
+++ src/pages/Report/Report.jsx
@@ -1,11 +1,9 @@
-import Header from '../../components/mypage/Header';
 import PictureMaterial from "../../components/Report/PictureMaterial";
 import QuillEditor from '../../components/Report/Quill';
 
 function Report() {
     return (
         <div className="edit">
-            <Header />
             <div className ="edit absolute left-0 h-screen w-1/2"
                  style={{paddingTop: '60px'}}>
                 <QuillEditor/>
