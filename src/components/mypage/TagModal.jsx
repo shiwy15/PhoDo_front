56d4535a5ef71c5c49d22bcacf69128514e13c0e
hodee <tlsghk8619@gmail.com>
2023-07-04 16:25:00 +0900

    delte picture

--- src/components/mypage/TagModal.jsx
+++ src/components/mypage/TagModal.jsx
@@ -200,7 +200,7 @@ const TagModal = ({ showModal, setShowModal, selectedImage }) => {
                   }}
                 />  
                
-                <button type="button" onclick={handleImageDelete} class="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full 
+                <button type="button" onClick={handleImageDelete} class="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full 
                 text-sm px-3 py-2 text-center mt-10
                 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
                 사진 삭제</button>
