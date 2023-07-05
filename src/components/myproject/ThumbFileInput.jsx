816ea1098dbd1b7280877cb5c3ef8fa52ccb1be8
jinkyo <wlsry0910@gmail.com>
2023-07-05 17:37:18 +0900

    feat: pypage css

--- src/components/myproject/ThumbFileInput.jsx
+++ src/components/myproject/ThumbFileInput.jsx
@@ -43,7 +43,7 @@ const ThumbFileInput = ({projectId, defThumb}) => {
   };
 
   return (
-    <div className="p-4 shadow-4 rounded-lg w-full m-4 h-full" style={{"margin": 'auto'}}>
+    <div className="p-4 shadow-4 rounded-lg w-full h-full" style={{"margin": 'auto'}}>
       <h2 className="text-2xl font-semibold pb-6 relative top-0 text-center">변경할 썸네일을 입력해주세요.</h2>
 
       {/* 🌿 입력된 이미지의 리스트를 보여주는 창 */}
@@ -72,13 +72,13 @@ const ThumbFileInput = ({projectId, defThumb}) => {
           data-te-ripple-init
           data-te-ripple-color="light"
           onClick={handleUpload}
-          className="w-32 inline-block bg-purple-700 rounded bg-primary px-6 pb-2 pt-2.5 text-md font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+          className="w-32 inline-block bg-purple-700 rounded px-6 pb-2 pt-2.5 text-lg font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
           style={{
               display: "flex",
               alignItems: "center",
               justifyContent: "center",
           }}>
-          Upload
+            업로드
         </button>
       </div>
     </div>
