650e48964c5e663a1ab4b16c7c8cae4407c6e394
shiwy15 <shiwy15@gmail.com>
2023-07-05 15:25:29 +0900

    사진 노드 사이즈 조절 에러 수정

--- src/components/Editor/Node/LazyPicNode.js
+++ src/components/Editor/Node/LazyPicNode.js
@@ -3,7 +3,7 @@ import { Handle, Position,  } from 'reactflow';
 import { NodeResizer } from '@reactflow/node-resizer';
 import { memo } from 'react';
 
-const LazyPicNode = ({ id, selected, data }) => {
+const LazyPicNode = ({ id, selected, data, isConnectable }) => {
   const handleStyle = {
     background: 'red',
     border: '2px solid white',
@@ -42,12 +42,16 @@ const LazyPicNode = ({ id, selected, data }) => {
   return (
     <div className="PictureNodeblock" style={{ border: '4px solid black', borderRadius: '6px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
       <div style={{ position: 'relative', width: '100%' }}>
-        <NodeResizer isVisible={selected} minWidth={180} minHeight={100} handleStyle={handleStyle} />
-        <Handle type="target" position={Position.Left} />
-        <div>
-          <img src={imgSrc} alt='thumbnail' style={{ height: '400px', objectFit: 'contain' }} />
-        </div>
-        <Handle type="source" position={Position.Right} />
+        <Handle type="target" position={Position.Top} id="top" isConnectable={isConnectable} />
+        <Handle type="target" position={Position.Left} id="left" isConnectable={isConnectable} />
+        <NodeResizer color="#ff0071" isVisible={selected} minWidth={180} minHeight={100} style={{ position: 'absolute', top: 0, right: 0, bottom: 0, left: 0 }} handleStyle={handleStyle} >
+          <Handle type="target" position={Position.Left}/>
+          <Handle type="source" position={Position.Right}/>
+        </NodeResizer>
+        
+          <img src={imgSrc} alt='thumbnail' style={{  width: '100%', height: '100%', objectFit: 'contain' }} />
+          <Handle type="source" position={Position.Right} id="right" isConnectable={isConnectable} />
+        <Handle type="source" position={Position.Bottom} id="bottom" isConnectable={isConnectable}/>
       </div>
     </div>
   );
