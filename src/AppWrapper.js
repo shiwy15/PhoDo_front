3dc09a3edd16f1de583cc09c47924c5916594304
hodee <tlsghk8619@gmail.com>
2023-07-06 02:04:47 +0900

    feat: report change & waiting

--- src/AppWrapper.js
+++ src/AppWrapper.js
@@ -16,7 +16,7 @@ const AppWrapper = ({ children }) => {
 
       const timer = setTimeout(() => {
         updateLoad(false);
-      }, 5000);
+      }, 7000);
 
       return () => clearTimeout(timer);
     } else {
