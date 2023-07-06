7bbe9bea288d6d816ae6beee53a4c9a9537dab0d
shiwy15 <shiwy15@gmail.com>
2023-07-07 01:44:55 +0900

    레포트 수정

--- src/components/Report/Quill.jsx
+++ src/components/Report/Quill.jsx
@@ -52,6 +52,7 @@ const QuillEditor = () => {
 
     useEffect(() => {
         const contentHtml = `
+        <div>
         <br>
         <h1><span style="color: rgb(153, 51, 255);">아파트 내부 인테리어에 관한 가족회의 보고서 </span></h1>
         <br>
@@ -59,26 +60,31 @@ const QuillEditor = () => {
         <p class="ql-align-right"><strong>회의 참석자:</strong> 가족 전체</p>
         <br>
         <h2>1. 인테리어 필요성</h2>
-        
-        <p>우리 가족 아파트의 인테리어 개선은 단순히 물질적인 만족도만을 증가시키는 것이 아닙니다. 보다 편안하고, 아름다운 환경에서 생활하는 것은 우리 가족 모두의 삶의 질을 향상시키고, 행복감을 늘려줄 것이라고 확신하고 있습니다. 이러한 생각은 우리 가족 모두가 공감하고 있으며, 따라서 이번 내부 인테리어는 가족 모두의 기대와 열정으로 시작되었습니다.</p>
+        <br>
+        <p style="margin-left: 1rem;">우리 가족 아파트의 인테리어 개선은 단순히 물질적인 만족도만을 증가시키는 것이 아닙니다. 보다 편안하고, 아름다운 환경에서 생활하는 것은 우리 가족 모두의 삶의 질을 향상시키고, 행복감을 늘려줄 것이라고 확신하고 있습니다. 이러한 생각은 우리 가족 모두가 공감하고 있으며, 따라서 이번 내부 인테리어는 가족 모두의 기대와 열정으로 시작되었습니다.</p>
         <br>
         <h2>2. 인테리어 계획</h2>
-        
+        <br>
         <p>우리 가족은 각 방에 따라 다른 인테리어 계획을 세우기로 결정했습니다. 이는 각 방의 기능과 사용목적, 그리고 가족 구성원의 개인적인 취향을 고려하기 위함입니다.</p>
-        <h3>2-a. 거실</h3>
+        <br>
+        <h3><strong>2-a. 거실</strong></h3>
         <p>거실은 가족이 함께 모여 대화하고, 휴식을 취하는 공간입니다. 이에 걸맞게 벽지는 흐린 초록색으로, 바닥은 헤링본 패턴의 진한 갈색으로 결정하였습니다. 이러한 색조는 평온하고 따뜻한 분위기를 더해줄 것입니다. 또한, 편안한 환경을 위해 하얀색 소파를 구매할 예정이며, 식물 그림 액자를 최소 3개 이상 구매하여 신선함을 더할 예정입니다.</p>
-        <h3>2-b. 침실</h3>
+        <br>
+        <h3><strong>2-b. 침실</strong></h3>
         <p>침실은 가장 개인적인 공간으로, 편안하고 쾌적한 환경이 필요합니다. 필요한 가구로는 침대, 옷장, 테이블, 의자, 선반, 조명이 있으며, 이에 대한 예산은 300만원으로 설정하였습니다.</p>
-        <h3>2-c. 손님방</h3>
+        <br>
+        <h3><strong>2-c. 손님방</strong></h3>
         <p>손님방은 가족의 환대의 표현이기 때문에, 침대 및 여분의 이불을 준비하여 손님들이 편안하게 머무를 수 있도록 할 예정입니다.</p>
-        <h3>2-d. 화장실</h3>
+        <br>
+        <h3><strong>2-d. 화장실</strong></h3>
         <p>화장실은 깨끗하고 신선한 이미지를 유지하는 것이 중요합니다. 예산 한도는 600만원으로 설정하였으며, 깔끔함을 강조하기 위해 타일은 하얀색으로 결정하였습니다.</p>
         <br>
         <h2>3. 추가 구매가 필요한 가구 및 도구들</h2>
+        <br>
         <p>위의 계획을 실행하기 위해 액자, 쿠션, 4인용 식탁, 조명 등을 추가로 구매할 예정입니다. 이들 가구와 도구는 각 방의 분위기를 더욱 풍성하게 하고, 가족들의 편의를 증진시킬 것입니다.</p>
         
         <p>이번 내부 인테리어는 우리 가족의 삶의 질을 향상시키고, 더욱 행복한 생활 환경을 만들어 주기 위한 첫걸음입니다. 가족 모두의 공동 노력으로 이를 성공적으로 완수할 수 있기를 바랍니다.</p>
-    
+        </div>
         `;
     
         setValue(contentHtml);
@@ -115,7 +121,7 @@ const QuillEditor = () => {
             // const pdfWidth = pdf.internal.pageSize.getWidth();
             // const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
             
-            pdf.addImage(imgData, 'PNG', 10, 0, pdfWidth, pdfHeight);
+            pdf.addImage(imgData, 'PNG', 10, 8, pdfWidth, pdfHeight);
             pdf.line(15, 8, 195, 8);
             pdf.line(15, 289, 195, 289);
             pdf.save("download.pdf");
@@ -123,7 +129,7 @@ const QuillEditor = () => {
     };    
 
 return (
-<div className='py-2'>
+<div className='py-2' >
 <div style={{ display: 'flex', justifyContent: 'center' }}>
         <button 
             className="button mr-3 inline-block mb-2 bg-purple-700 rounded px-6 pb-2 pt-2.5 text-md font-medium uppercase leading-normal text-white" 
